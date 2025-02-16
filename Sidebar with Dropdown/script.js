df760ab89d962e1926e3c92db5c9abe036a22dc8
cui-zi <sun-cui-zi@protonmail.com>
2025-02-16 22:24:10 +0800

    finish Sidebar with dropdown  Menu

--- Sidebar with Dropdown/script.js
+++ Sidebar with Dropdown/script.js
@@ -4,6 +4,13 @@ const toggleDropdown = (dropdown, menu, isOpen) => {
   menu.style.height = isOpen ? `${menu.scrollHeight}px` : 0;
 };
 
+// Close all open dropdowns 
+const closeAllDropdowns = () => {
+  document.querySelectorAll(".dropdown-container.open").forEach(openDropdown => {
+    toggleDropdown(openDropdown, openDropdown.querySelector(".dropdown-menu"), false);
+  })
+}
+
 // Attach click event to all dropdown toggles
 document.querySelectorAll(".dropdown-toggle").forEach((dropdownToggle) => {
   dropdownToggle.addEventListener("click", (e) => {
@@ -13,11 +20,17 @@ document.querySelectorAll(".dropdown-toggle").forEach((dropdownToggle) => {
     const menu = dropdown.querySelector(".dropdown-menu");
     const isOpen = dropdown.classList.contains("open");
 
-    toggleDropdown(dropdown, menu, !isOpen);
+    closeAllDropdowns(); // Close all open dropdowns
+
+    toggleDropdown(dropdown, menu, !isOpen); // Toggle current dropdown visibility
   });
 });
 
-document.querySelector(".sidebar-toggle").addEventListener("click", () => {
+document.querySelectorAll(".sidebar-toggle, .sidebar-menu-button").forEach(button => button.addEventListener("click", () => {
+  closeAllDropdowns();
+
   // Toggle collpased class on sidebar
   document.querySelector(".sidebar").classList.toggle("collapsed");
-});
+}));
+
+if(window.innerWidth <= 1024) document.querySelector(".sidebar").classList.toggle("collapsed")
